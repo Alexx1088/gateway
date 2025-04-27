c301f7f985035c4965754aedb06e133fe098e648
Alexx108 <alexx1088@gmail.com>
2025-04-27 21:17:25 +0500

    first commit

---
+++ cmd/gateway/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	authpb "github.com/Alexx1088/authservice/proto"
+	userpb "github.com/Alexx1088/userservice/proto"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"log"
+	"net/http"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	// Connect to AuthService
+	err := authpb.RegisterAuthServiceHandlerFromEndpoint(ctx, mux, "localhost:50052", opts)
+	if err != nil {
+		log.Fatalf("failed to connect to AuthService: %v", err)
+	}
+
+	// Connect to UserService
+	err = userpb.RegisterUserServiceHandlerFromEndpoint(ctx, mux, "localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("failed to connect to UserService: %v", err)
+	}
+
+	log.Println("gateway server starting on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("failed to start HTTP server: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/Alexx1088/gateway
+
+go 1.24.1
+
+require (
+	github.com/Alexx1088/authservice v0.0.0-20250427154811-eb7dd6b7ff5c
+	github.com/Alexx1088/userservice v0.0.0-20250426175933-0051022d10d9
+)
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
+	google.golang.org/grpc v1.72.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
